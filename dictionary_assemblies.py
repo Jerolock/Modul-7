0b67183d1a9a0521265d413228bd4e8224912fe7
Jerolock <163981038+Jerolock@users.noreply.github.com>
2024-06-04 12:13:11 +0500

    Update dictionary_assemblies.py

--- dictionary_assemblies.py
+++ dictionary_assemblies.py
@@ -6,7 +6,7 @@ def odd(x):
     return x % 2
 
 
-dict1 = [1, 2, 5, 12, 11, 35, 4, 89, 10]
+dict1 = [1, 2, 5, 7, 12, 11, 35, 4, 89, 10]
 
 result = map(squar, filter(odd,dict1))
 print(list(result))
